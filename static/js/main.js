79ee3ac80df080ff387c3728ff869f9838493d89
Rezwanul-Haque <rezwanul.cse@gmail.com>
2019-04-11 01:00:06 +0600

    Now notes can added to side bar.

--- static/js/main.js
+++ static/js/main.js
@@ -6,7 +6,57 @@ new Vue({
     // Some data
     data() {
         return {
-            content: 'This is a note.'
+            content: "I'm in **bold**!",
+            // New! A note array
+            notes: [],
         }
+    },
+    // Computed properties
+    computed: {
+        notePreview (){
+            // Markdown rendered to HTML
+            return marked(this.content)
+        },
+    },
+    addButtonTitle () {
+        return notes.length + ' note(s) already'
+    },
+    // Chage watchers
+    watch: {
+        // Watchig 'content' data property
+        content: 'saveNote',
+        },
+    // DRY content- Don't repeat yourself
+    methods: {
+        // vue function
+        saveNote () {
+            console.log('saving note: ', this.content)
+            localStorage.setItem('content', this.content)
+            this.reportOperation('saving')
+        },
+        // Add a note with some default content and select it
+        addNote () {
+            const time = Date.now()
+            // Default new note
+            const note = {
+                id: String(time),
+                title: 'New note ' + (this.notes.length + 1),
+                content: "**Hi!** This notebook is using [markdown](https://github.com/adam-p/markdown-here/wiki/Markdown-Cheatsheet) for formatting!",
+                created: time,
+                favorite: false,
+            }
+            // Add to the list
+            this.notes.push(note)
+        },
+        
+        reportOperation (opName) {
+            console.log('The', opName, 'operation was completed!')
+        },
+    },
+    // This will be called when the instance is ready - called is a hook
+    created() {
+        // Set the content to the stored value
+        // or to a default string if nothing was saved
+        this.content = localStorage.getItem('content') || 'You can write in **markdown**'
     },
 })
